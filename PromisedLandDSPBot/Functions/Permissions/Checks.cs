28853baaca4b3d4901bf5068b7ea376c9ca73893
MrDevBot <alexeldera04@gmail.com>
2023-03-18 16:57:47 +1100

    streamline implementation of common checks

---
+++ PromisedLandDSPBot/Functions/Permissions/Checks.cs
@@ -0,0 +1,36 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+
+namespace PromisedLandDSPBot.Functions.Permissions;
+
+public class Checks
+{
+    public static bool RejectDM(InteractionContext ctx)
+    {
+        if (!ctx.Channel.IsPrivate) return false;
+        ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+            .WithContent(":Warning: This command cannot be used in a DM channel."));
+        return true;
+
+    }
+    
+    public static bool RejectGuild(InteractionContext ctx)
+    {
+        if (ctx.Channel.IsPrivate) return false;
+        ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+            .WithContent(":Warning: This command cannot be used in a guild."));
+        return true;
+
+    }
+    
+    public static bool RejectNonDeveloper(InteractionContext ctx)
+    {
+        if (ctx.Client.CurrentApplication.Owners.Contains(ctx.User)) return false;
+        ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+            .WithContent(":Warning: This command can only be used by the bot developer."));
+        return true;
+
+    }
+    
+}
