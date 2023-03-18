8717b9be3e4e43d9415a3506d49e95899d85e5b9
MrDevBot <alexeldera04@gmail.com>
2023-03-18 15:50:53 +1100

    add say, embed, reply commands to debug module

--- PromisedLandDSPBot/modules/Debug/Module.cs
+++ PromisedLandDSPBot/modules/Debug/Module.cs
@@ -1,18 +1,23 @@
 ﻿using DSharpPlus;
+
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+
 using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
+
 using static System.Reflection.Assembly;
 
 namespace PromisedLandDSPBot.Modules.Debug;
+
+
 /// <summary>
-/// Bot-specific debug commands - this is supposed to be used by bot admins to test functionality and debug errors.
+/// Bot-specific debug commands - this is supposed to be used by bot developers to test functionality and debug errors.
 /// </summary>
 public class Module
 {
     //slash command implementations
-    [SlashCommandGroup("info", "Debug Functions - Special Access Only")]
+    [SlashCommandGroup("dev", "Debug Functions - Special Access Only")]
     public class Slash : ApplicationCommandModule
     {
         [SlashCommand("ping", "the latency between the bot and discord")]
@@ -38,33 +43,106 @@ public class Module
                 }
                 .AddField("Entry Point", $"`{GetExecutingAssembly().EntryPoint!.Name}`", true)
                 .AddField("Loaded Modules", $"{GetExecutingAssembly().Modules.Aggregate(string.Empty, (current, module) => $"{current}\n`{module}`")}", true)
-                .AddField("Developers", Constants.Developers.Aggregate(string.Empty, (current, developer) => $"{current}\n{developer}"), true)
+                .AddField("Developers", ctx.Client.CurrentApplication.Owners.Aggregate(string.Empty, (current, Developer) => current + (Developer.Username + "#" + Developer.Discriminator)), true)
                 .AddField("Version", Constants.Version, true)
                 //.AddField("In Line", "This is in line", true)
                 .Build();
-            
-                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
-                new DiscordInteractionResponseBuilder().AddEmbed(de));
+
         }
 
-        [SlashCommand("modaltest", "Made to test how modals work in this framework.")]
-        public async Task ModalTestCommand(InteractionContext ctx,
-            [Option("title", "The title of the Modal")]
-            string title = "Modal Title")
+        [SlashCommand("Say", "developer command, not available to the public")]
+        public async Task Say(InteractionContext ctx, [Option("message", "the message to send")] string message)
         {
-            var response = new DiscordInteractionResponseBuilder();
-            response
-                .WithTitle("Super cool modal!")
-                .WithCustomId("my-modal")
-                .AddComponents(new TextInputComponent(label: "Favorite food", customId: "fav-food",
-                    placeholder: "Pizza, Icecream, etc", max_length: 30))
-                .AddComponents(new TextInputComponent("Why?", "why-fav", "Because it tastes good", required: false,
-                    style: TextInputStyle.Paragraph));
+            if (ctx.Client.CurrentApplication.Owners.Contains(ctx.User))
+            {
+                await ctx.Client.SendMessageAsync(ctx.Channel, message);
 
-            await ctx.CreateResponseAsync(InteractionResponseType.Modal, response);
+                await ctx.CreateResponseAsync("Message Sent.", true);
+
+                await Task.Delay(2000);
+                await ctx.DeleteResponseAsync();
+            }
+            else
+            {
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                    new DiscordInteractionResponseBuilder().WithContent(":warning: you do not have permission to use this command. you are not a registered developer of this application."));
+            }
 
         }
 
+        [SlashCommand("Embed", "developer command, not available to the public")]
+        public async Task Embed(InteractionContext ctx,
+            [Option("Title", "the title of the embed")] string title = "Title",
+            [Option("Description", "the description of the embed")]
+            string description = "Description")
+        {
+
+            if (ctx.Client.CurrentApplication.Owners.Contains(ctx.User))
+            {
+                var de = new DiscordEmbedBuilder()
+                    {
+                        Color = DiscordColor.Blurple,
+                        Author = new DiscordEmbedBuilder.EmbedAuthor()
+                        {
+                            IconUrl = ctx.Client.CurrentUser.AvatarUrl,
+                            Name = ctx.Client.CurrentUser.Username,
+                        },
+
+                        Title = title, Description = description,
+                        Footer = new DiscordEmbedBuilder.EmbedFooter()
+                            { Text = $"Current Application: {ctx.Client.CurrentApplication.Name}" }
+                    }
+                    .Build();
+                
+                await ctx.Client.SendMessageAsync(ctx.Channel, de);
+
+                await ctx.CreateResponseAsync("Embed Sent.", true);
+
+                await Task.Delay(2000);
+                await ctx.DeleteResponseAsync();
+            }
+            else
+            {
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                    new DiscordInteractionResponseBuilder().WithContent(":warning: you do not have permission to use this command. you are not a registered developer of this application."));
+            }
+        }
+        
+        [SlashCommand("Reply", "developer command, not available to the public")]
+        public async Task Reply(InteractionContext ctx, [Option("id", "the message id to reply to")] string message, [Option("reply", "the message to send")] string reply)
+        {
+            // try to cast the "message" string as a ulong
+            if (!ulong.TryParse(message, out var id))
+            {
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                    new DiscordInteractionResponseBuilder().WithContent(":warning: invalid message id."));
+
+                await Task.Delay(2000);
+                await ctx.DeleteResponseAsync();
+                
+                return;
+            }
+            
+            if (ctx.Client.CurrentApplication.Owners.Contains(ctx.User))
+            {
+                var builder = new DiscordMessageBuilder();
+                builder.WithReply(ulong.Parse(message), true);
+                builder.WithContent(reply);
+                
+                await ctx.Client.SendMessageAsync(ctx.Channel, builder);
+                
+                await ctx.CreateResponseAsync("Reply Sent.", true);
+
+                await Task.Delay(2000);
+                await ctx.DeleteResponseAsync();
+            }
+            else
+            {
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                    new DiscordInteractionResponseBuilder().WithContent(":warning: you do not have permission to use this command. you are not a registered developer of this application."));
+            }
+
+        }
         
     }
 }
