ead44046812967134ca25a09ac61eb69985b13fd
MrDevBot <Alexeldera04@gmail.com>
2023-05-06 16:29:01 +1000

    create ticket close command stub

--- PromisedLandDSPBot/modules/Tickets/Module.cs
+++ PromisedLandDSPBot/modules/Tickets/Module.cs
@@ -3,6 +3,8 @@ using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
+using LiteDB;
+using LiteDB.Async;
 
 namespace PromisedLandDSPBot.Modules.Tickets;
 
@@ -53,6 +55,54 @@ public class Module
             await ctx.CreateResponseAsync(InteractionResponseType.Modal, response);
         */
             
+        }
+        
+        [SlashCommand("close", "close the current ticket, can only be used in ticket channels"), RequireGuild()]
+        public async Task CloseTicket(InteractionContext ctx)
+        {
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                new DiscordInteractionResponseBuilder().WithContent(":warning: this feature is still under development."));
+
+            await Task.Delay(2000);
+            await ctx.DeleteResponseAsync();
+        }
+
+        class Ticket
+        {
+            internal Ticket()
+            {
+                db = new LiteDatabaseAsync("Filename=Conversations.db;Connection=shared;");
+                epoch = DateTime.UtcNow;
+                lastAccess = DateTime.UtcNow;
+            }
+
+            // the last time the conversation was accessed
+            private DateTime lastAccess;
+        
+            // the time at which the conversation was instantiated into memory
+            private readonly DateTime epoch;
+        
+            // database reference
+            private LiteDatabaseAsync db;
+
+            public class RequestResponse
+            {
+                // the id of the conversation
+                public ObjectId TicketId { get; set; }
+            
+                // the users request
+                public ulong TicketChannel { get; set; }
+            
+                // the models response
+                public ulong TicketCreator { get; set; }
+            
+                // creation time of the object
+                public DateTime Time { get; set; }
+
+                // message history of the ticket
+                public List<String> History { get; set; }
+            }
+
         }
     }
 }
