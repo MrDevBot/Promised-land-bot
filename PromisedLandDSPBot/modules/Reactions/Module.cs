df29ef2ea22cd94232463f5bb5977ad573428a2e
Alexander Elder <Alexeldera04@gmail.com>
2023-12-27 19:01:38 +1000

    Update Config usage in Ticket module

--- PromisedLandDSPBot/modules/Reactions/Module.cs
+++ PromisedLandDSPBot/modules/Reactions/Module.cs
@@ -6,6 +6,8 @@ namespace PromisedLandDSPBot.Modules.Reactions;
 
 public class Module
 {
+    private Config _config = Program.Config;
+    
     //slash command implementations
     [SlashCommandGroup("reactionrole", "For making, editing and deleting reaction role structures."), RequireGuild()]
     public class Slash : ApplicationCommandModule
