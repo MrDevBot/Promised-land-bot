e5ea637ecaa32f0dc0cf6ee08e19f2c443f222c2
Alexander Elder <Alexeldera04@gmail.com>
2023-12-27 19:01:21 +1000

    Update config usage in Triggers module

--- PromisedLandDSPBot/modules/Triggers/Module.cs
+++ PromisedLandDSPBot/modules/Triggers/Module.cs
@@ -6,6 +6,8 @@ namespace PromisedLandDSPBot.Modules.Triggers;
 
 public class Module
 {
+    private Config _config = Program.Config;
+    
     //slash command implementations
     [SlashCommandGroup("trigger", "For setting, modifying and deleting triggers (@role pings)."), RequireGuild()]
     public class Slash : ApplicationCommandModule
